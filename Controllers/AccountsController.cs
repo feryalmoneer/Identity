30fd1c7f200a1055b8c139872941d7854ee2417c
zain_moneer <zain_moneer@ZAIN-M0NEER>
2024-09-05 15:45:30 +0300

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,70 @@
+﻿using Identity.Data;
+using Identity.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public readonly ApplicationDbContext context;
+        public readonly UserManager<IdentityUser> userManager;
+        public readonly SignInManager<IdentityUser> signInManager;
+
+
+        public AccountsController(ApplicationDbContext context,   UserManager<IdentityUser> userManager , SignInManager<IdentityUser>signInManager)
+        {
+            this.userManager = userManager;
+            this.context = context;
+            this.signInManager = signInManager;
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> Register(RegisterViewModel model)
+        {
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                PhoneNumber = model.Phone,
+                UserName = model.Email, 
+            };
+
+          var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>  Login(LoginViewModel model)
+        {
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index" , "Home");
+
+            }
+            return View(model);
+
+        }
+    }
+}
---
+++ Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(25)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(25)]
+        [Compare (nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        public string Phone { get; set; }
+    }
+}
